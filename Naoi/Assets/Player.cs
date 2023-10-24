2faac4051c5b08aab7ade83411e36f701312301a
thanhx99 <trungle9995@gmail.com>
2023-10-24 09:56:02 -0400

    minor changes

--- Naoi/Assets/Player.cs
+++ Naoi/Assets/Player.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Compression;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public CharacterController playerController;
+    public float speed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
+
+    //method controls the movement
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 movement = x * transform.right + y* transform.forward;
+        movement = movement * speed *Time.deltaTime;
+        playerController.Move(movement);
+    }
+
 }
